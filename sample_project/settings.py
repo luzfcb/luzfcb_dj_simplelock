4c2e6244ff035894f651784179cafb338d3b86d4
Fabio C. Barrioneuvo da Luz <bnafta@gmail.com>
2016-02-17 17:03:42 -0300

    renomeado appconfig

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sample_project.app_test',
-    'luzfcb_dj_simplelock',
+    'luzfcb_dj_simplelock.apps.DjSimpleLockAppConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
